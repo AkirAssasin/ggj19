3f100387c3a34a7bc860004df8f66bc91d777be2
DESKTOP-0P40I8U\msi <hwakint94@yahoo.com>
2019-01-27 00:28:15 +0800

    Fox Levels and some minor changes to the main menu level

---
+++ Assets/Scripts/FoxLevelScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxLevelScript : MonoBehaviour 
+{
+	public GameObject key;
+	public GameObject keyLock;
+
+	void Awake()
+	{
+		key = GameObject.FindGameObjectWithTag("Key");
+		keyLock = GameObject.Find("KeyLock");
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Key")
+		{
+			Destroy(key);
+			Destroy(keyLock);
+		}
+	}
+}
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour 
+{
+
+	public GameObject creditScreen;
+
+	// Use this for initialization
+	void Start () 
+	{
+		PlayBgm();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void StartGame()
+	{
+		
+	}
+
+	public void Credits()
+	{
+		if(!creditScreen.activeSelf)
+		{
+			creditScreen.SetActive(true);
+		}
+		else if(creditScreen.activeSelf)
+		{
+			creditScreen.SetActive(false);
+		}
+	}
+
+	public void ChangeScene(string name)
+	{
+		SceneManager.LoadScene(name);
+	}
+
+	void PlayBgm()
+	{
+		SoundManagerScript.Instance.PlayLoopingBGM(AudioClipID.BGM_MAIN_MENU);
+	}
+}
