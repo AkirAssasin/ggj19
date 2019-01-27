4305181e2665409c10f431de1c264d1c5f661961
DESKTOP-0P40I8U\msi <hwakint94@yahoo.com>
2019-01-27 14:26:53 +0800

    BGM and SFX added for deer and whale scene

---
+++ Assets/Scripts/BGMScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		SoundManagerScript.Instance.PlayLoopingBGM(AudioClipID.BGM_Level_1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
